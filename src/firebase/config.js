155dd6817ece77ffb82e84eaa140434e69be04b8
Ryuichi <bounceback.kanata@gmail.com>
2021-04-08 07:41:44 +0900

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyAyw2uT483W-jPhNgYpLAmma0YsgICSoF8",
+    authDomain: "ec-app-5cc1b.firebaseapp.com",
+    projectId: "ec-app-5cc1b",
+    storageBucket: "ec-app-5cc1b.appspot.com",
+    messagingSenderId: "1038250391885",
+    appId: "1:1038250391885:web:3e04433cc01ea2056d1f04",
+    measurementId: "G-X83453TV83"
+  };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {Provider} from 'react-redux';
+import createStore from './reducks/store/store';
+import {ConnectedRouter} from 'connected-react-router';
+import * as History from 'history';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const history = History.createBrowserHistory();
+export const store = createStore(history);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+      <ConnectedRouter history={history}>
+          <App />
+      </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
